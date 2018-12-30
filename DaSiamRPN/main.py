2ae59e67b1fe3f435736288c4c96f4cd0df25f3f
JachinShen <jachinshen@foxmail.com>
2018-12-30 10:27:37 +0800

    add time

--- DaSiamRPN/main.py
+++ DaSiamRPN/main.py
@@ -17,6 +17,7 @@ import numpy as np
 import torch
 import cv2  # imread
 import sys
+import time
 
 print("Start DaSiamRPN")
 
@@ -41,6 +42,7 @@ res_dict = {}
 overlap = []
 for k, (seqname, seq) in enumerate(data.items()):
     print("Processing " + seqname)
+    toc=0
     rect_list = []
     img_list = seq['images']
     gt = seq['gt']
@@ -53,7 +55,9 @@ for k, (seqname, seq) in enumerate(data.items()):
     target_pos, target_sz = np.array([cx, cy]), np.array([w, h])
     image_file = os.path.join(img_dir, img_list[0])
     im = cv2.imread(image_file)  # HxWxC
+    tic = time.time()
     state = SiamRPN_init(im, target_pos, target_sz, net, gt[0])  # init tracker
+    toc += time.time() - tic
 
     # tracking
     for i in range(len(img_list)):
@@ -61,11 +65,15 @@ for k, (seqname, seq) in enumerate(data.items()):
         if not image_file:
             break
         im = cv2.imread(image_file)  # HxWxC
+        tic = time.time()
         state = SiamRPN_track(state, im)  # track
+        toc += time.time() - tic
         res = cxy_wh_2_rect(state['target_pos'], state['target_sz'])
         rect_list.append(res.tolist())
         overlap.append(overlap_ratio(res, gt[i]))
 
     # store results
+    print("fps: {.2f}".format(len(gt)/toc))
     res_dict[seqname] = rect_list
+    res_dict[seqname+"time"] = [toc, len(gt), len(gt)/toc]
     json.dump(res_dict, open("../results/DaSiamRPN.json", 'w'), indent=2)
--- DaSiamRPNAT2/main.py
+++ DaSiamRPNAT2/main.py
@@ -5,21 +5,21 @@
 # --------------------------------------------------------
 #!/usr/bin/python
 
-import sys
-import cv2  # imread
-import torch
-import numpy as np
-import matplotlib.pyplot as plt
-import json
-import pickle
-import os
-from os.path import realpath, dirname, join
-
-from net import SiamRPNBIG
-from run_SiamRPN import SiamRPN_init, SiamRPN_track
 from utils import get_axis_aligned_bbox, cxy_wh_2_rect, overlap_ratio, draw_rect
+from run_SiamRPN import SiamRPN_init, SiamRPN_track
+from net import SiamRPNBIG
+from os.path import realpath, dirname, join
+import os
+import pickle
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import cv2  # imread
+import sys
+import time
 
-print("Start DaSiamRPNAT")
+print("Start DaSiamRPN")
 
 # load net
 net_file = "../model/SiamRPNBIG.model"
@@ -29,12 +29,12 @@ net.eval().cuda()
 
 # warm up
 for i in range(10):
-    net.temple(torch.autograd.Variable(torch.FloatTensor(1, 3, 127, 127)).cuda())
+    net.temple(torch.autograd.Variable(
+        torch.FloatTensor(1, 3, 127, 127)).cuda())
     net(torch.autograd.Variable(torch.FloatTensor(1, 3, 255, 255)).cuda())
 
 img_home = '../dataset/OTB/'
 data_path = '../data/vot-otb.pkl'
-print("open vot-otb.pkl")
 with open(data_path, 'rb') as fp:
     data = pickle.load(fp)
 
@@ -42,6 +42,7 @@ res_dict = {}
 overlap = []
 for k, (seqname, seq) in enumerate(data.items()):
     print("Processing " + seqname)
+    toc=0
     rect_list = []
     img_list = seq['images']
     gt = seq['gt']
@@ -49,20 +50,30 @@ for k, (seqname, seq) in enumerate(data.items()):
     gt_init = gt[0]
     x, y, w, h = gt_init[0], gt_init[1], gt_init[2], gt_init[3]
     cx, cy = x + w//2, y + h // 2
-    
+
+    # tracker init
     target_pos, target_sz = np.array([cx, cy]), np.array([w, h])
     image_file = os.path.join(img_dir, img_list[0])
     im = cv2.imread(image_file)  # HxWxC
+    tic = time.time()
     state = SiamRPN_init(im, target_pos, target_sz, net, gt[0])  # init tracker
+    toc += time.time() - tic
+
+    # tracking
     for i in range(len(img_list)):
         image_file = os.path.join(img_dir, img_list[i])
         if not image_file:
             break
         im = cv2.imread(image_file)  # HxWxC
+        tic = time.time()
         state = SiamRPN_track(state, im)  # track
+        toc += time.time() - tic
         res = cxy_wh_2_rect(state['target_pos'], state['target_sz'])
         rect_list.append(res.tolist())
         overlap.append(overlap_ratio(res, gt[i]))
 
+    # store results
+    print("fps: {.2f}".format(len(gt)/toc))
     res_dict[seqname] = rect_list
-    json.dump(res_dict, open("../results/DaSiamRPNAT11.json", 'w'), indent=2) 
+    res_dict[seqname+"time"] = [toc, len(gt), len(gt)/toc]
+    json.dump(res_dict, open("../results/DaSiamRPNAT2.json", 'w'), indent=2)
